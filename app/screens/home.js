ba4ce120030bef1d6aace706041f0fdbdbdf0b8b
Kerkeb Zakaria <hikkary@macbook-air-de-kerkeb.home>
2018-11-10 20:23:30 +0100

    refacto(home): counter removed

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,10 +1,7 @@
-import { Button, StyleSheet, View } from 'react-native'
+import { Button } from 'react-native'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
-import Counter from '../components/counter'
-import CounterButton from '../components/counterButton'
 
 const BackgroundView = styled.View`
   background-color: #baffc9;
@@ -22,14 +19,6 @@ export default class App extends Component {
     navigation: PropTypes.object
   }
 
-  state = {
-    counterNb: 0
-  }
-
-  decrementCounter = () => {
-    this.setState({ counterNb: this.state.counterNb - 1 })
-  }
-
   // handle navigation
   handleCharactersButtonPress = () => {
     this.props.navigation.navigate('Characters')
@@ -39,18 +28,9 @@ export default class App extends Component {
     this.props.navigation.navigate('Options')
   }
 
-  incrementCounter = () => {
-    this.setState({ counterNb: this.state.counterNb + 1 })
-  }
-
   render() {
     return (
       <BackgroundView>
-        <Counter counterNb={this.state.counterNb} />
-        <ButtonContainer>
-          <CounterButton label="-" onPressFunction={this.decrementCounter} />
-          <CounterButton label="+" onPressFunction={this.incrementCounter} />
-        </ButtonContainer>
         <ButtonContainer>
           <Button title="Options" onPress={this.handleParameterButtonPress} />
           <Button
